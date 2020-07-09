608914d349070965f2608b171d32a0fc473ee16c
y__ <@tpad>
2020-07-10 00:01:39 +0100

    add animation

--- es5script.js
+++ es5script.js
@@ -4,11 +4,11 @@
 
 "use strict";	// "use strict" directive is new in ECMAScript version 5, ignored in <ECMA5, in ECMA6 it is default.
 
-// check function: do feature detection of newly introduced es6 syntaxes (Symbol objects, class keyword and arrow functions) 
+// check js version dependent feature support for es6 syntax (eg. Symbol objects, class keyword and arrow functions) 
 // 		this is done using the eval() function or other equivalents (e.g. Function() ), 
 //  	because writing invalid syntax will stop the script before its execution. 
 function checkJsFeatures() {
-    if (typeof Symbol == "undefined") return false;
+    //if (typeof Symbol == "undefined") return false;
     try {
         eval("class Foo {}");
         eval("var bar = (x) => x+1");
@@ -23,6 +23,101 @@ function checkJsFeatures() {
 function isStrictMode(){
     return !this;
 } 
+
+function checkAnimation(){
+	var domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
+		elm = document.getElementById( 'rotdemo' ),
+		message = document.getElementById( 'rottechnique' ),
+		rotate = 0,
+		animation = false,
+		transformation = false,
+		animationstring = 'animation',
+		transformstring = 'transform',
+		keyframeprefix = '',
+		pfx = '';
+		
+	// check if standard CSS3 animation is supported first
+	if( elm.style.animationName ) { animation = true; }    
+	if( elm.style.transform ) { transformation = true; }    
+
+	// else check if browser specific CSS animation is supported
+	if( animation === false ) {
+		for( var i = 0; i < domPrefixes.length; i++ ) {
+			if( elm.style[ domPrefixes[i] + 'AnimationName' ] !== undefined ) {
+				pfx = domPrefixes[ i ];
+				animationstring = pfx + 'Animation';
+				keyframeprefix = '-' + pfx.toLowerCase() + '-';
+				animation = true;
+				break;
+			}
+		}
+	}
+	if( transformation === false ) {
+		for( var i = 0; i < domPrefixes.length; i++ ) {
+			if( elm.style[ domPrefixes[i] + 'Transform' ] !== undefined ) {
+				pfx = domPrefixes[ i ];
+				transformstring = pfx + 'Transform';
+				transformation = true;
+				break;
+			}
+		}
+	}
+	
+	// add CSS animation to the element elm
+	if( animation === true ) {
+		message.innerHTML = 'Animated with CSS3 animation';
+
+		elm.style[ animationstring ] = 'rotate 1s linear infinite';
+
+		var keyframes = '@' + keyframeprefix + 'keyframes rotate { '+
+					'from {' + keyframeprefix + 'transform:rotate( 0deg ) }'+
+					'to {' + keyframeprefix + 'transform:rotate( 360deg ) }'+
+					'}';
+
+		if( document.styleSheets && document.styleSheets.length ) {
+			document.styleSheets[0].insertRule( keyframes, 0 );
+		} else {
+			var s = document.createElement( 'style' );
+			s.innerHTML = keyframes;
+			document.getElementsByTagName( 'head' )[ 0 ].appendChild( s );
+		}
+	}
+
+	// use js animation 	
+	if( animation === false ) {
+		message.innerHTML += 'Using an animation loop';
+		
+		if( requestAnimationFrame.toString().indexOf( 'mation' ) !== -1 ) {
+			message.innerHTML += ' with requestAnimationFrame';
+		} else {
+			message.innerHTML += ' without requestAnimationFrame';
+		}		
+		
+		if ( !window.requestAnimationFrame ) {
+			//Provides requestAnimationFrame in a cross browser way.
+			// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+			window.requestAnimationFrame = ( function() {
+				return window.webkitRequestAnimationFrame ||
+				window.mozRequestAnimationFrame ||
+				window.oRequestAnimationFrame ||
+				window.msRequestAnimationFrame ||
+				function( callback, fps ) {
+					window.setTimeout( callback, 1000 / fps );
+				};
+			} )();
+		}
+
+		if( transformation !== false ) {
+			loop();
+		} 
+	} 
+    
+	function loop() {
+		rotate += 4;
+		elm.style[ transformstring ] = 'rotate(' + rotate + 'deg)';
+		requestAnimationFrame( loop, 30 );
+	}
+}
 
 (function() {
 	
@@ -57,6 +152,8 @@ if (checkJsFeatures()) {
 	console.log("ES6 features NOT supported by this browser.");
 }
 
+
+
 function es5doIt(elemId, data){
 	var strstrict = '<br>Strict mode: ' + isStrictMode().toString();
 	var strfeatures = '<br>JS ECMA6 feature support: ' + checkJsFeatures().toString();
@@ -67,6 +164,7 @@ function es5doIt(elemId, data){
 	console.log(strmsg);
 	display(strmsg);		
 	
+	checkAnimation();
 
 	function display(msg) {
 		if(document.body) {				
